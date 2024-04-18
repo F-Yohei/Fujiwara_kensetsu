edcb453a86925ea89c29a7ca477f9c6cc8a2c08d
YoheiFukudome <pt83s0502@gmail.com>
2024-04-18 22:33:23 +0900

    404ページ、エントリーとお問合せのサンクスページのトップへ戻るボタンのhover時のアニメーション設定

--- _static/src/assets/js/modules/header.js
+++ _static/src/assets/js/modules/header.js
@@ -6,27 +6,6 @@ header
 ・スクロール時のアニメーション
 ==============================================================*/
 
-// var beforePos = 0;
-
-// function ScrollAnime() {
-//   var elemTop = $("#js-top").offset().top;
-//   var scroll = $(window).scrollTop();
-//   if (scroll == beforePos) {
-//   } else if (elemTop > scroll || 0 > scroll - beforePos) {
-//     $(".l-header").removeClass("js-hide");
-//     $(".l-header").addClass("js-view");
-//   } else {
-//     $(".l-header").removeClass("js-view");
-//     $(".l-header").addClass("js-hide");
-//   }
-
-//   beforePos = scroll;
-// }
-
-// $(window).scroll(function () {
-//   ScrollAnime();
-// });
-
 $(window).on("load resize", function () {
   let winW = $(window).width();
   let devW = 1025;
