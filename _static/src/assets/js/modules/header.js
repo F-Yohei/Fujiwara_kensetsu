763186f8c5c3c946b9792f251c733113edfdcbc0
YoheiFukudome <pt83s0502@gmail.com>
2024-04-18 22:23:37 +0900

    一度下にスクロールしてハンバーガーメニューを開くとナビゲーションメニューが非表示になっているバグを解消

--- _static/src/assets/js/modules/header.js
+++ _static/src/assets/js/modules/header.js
@@ -1,4 +1,4 @@
-import jQuery from 'jquery';
+import jQuery from "jquery";
 const $ = jQuery;
 
 /*==============================================================
@@ -6,23 +6,54 @@ header
 ・スクロール時のアニメーション
 ==============================================================*/
 
-var beforePos = 0;
+// var beforePos = 0;
 
-function ScrollAnime() {
-  var elemTop = $('#js-top').offset().top;
-  var scroll = $(window).scrollTop();
-  if (scroll == beforePos) {
-  } else if (elemTop > scroll || 0 > scroll - beforePos) {
-    $('.l-header').removeClass('js-hide');
-    $('.l-header').addClass('js-view');
+// function ScrollAnime() {
+//   var elemTop = $("#js-top").offset().top;
+//   var scroll = $(window).scrollTop();
+//   if (scroll == beforePos) {
+//   } else if (elemTop > scroll || 0 > scroll - beforePos) {
+//     $(".l-header").removeClass("js-hide");
+//     $(".l-header").addClass("js-view");
+//   } else {
+//     $(".l-header").removeClass("js-view");
+//     $(".l-header").addClass("js-hide");
+//   }
+
+//   beforePos = scroll;
+// }
+
+// $(window).scroll(function () {
+//   ScrollAnime();
+// });
+
+$(window).on("load resize", function () {
+  let winW = $(window).width();
+  let devW = 1025;
+  if (winW <= devW) {
+    //1024px以下の時の処理
+    return;
   } else {
-    $('.l-header').removeClass('js-view');
-    $('.l-header').addClass('js-hide');
+    //1025pxより大きい時は何もしない
+    var beforePos = 0;
+
+    function ScrollAnime() {
+      var elemTop = $("#js-top").offset().top;
+      var scroll = $(window).scrollTop();
+      if (scroll == beforePos) {
+      } else if (elemTop > scroll || 0 > scroll - beforePos) {
+        $(".l-header").removeClass("js-hide");
+        $(".l-header").addClass("js-view");
+      } else {
+        $(".l-header").removeClass("js-view");
+        $(".l-header").addClass("js-hide");
+      }
+
+      beforePos = scroll;
+    }
+
+    $(window).scroll(function () {
+      ScrollAnime();
+    });
   }
-
-  beforePos = scroll;
-}
-
-$(window).scroll(function () {
-  ScrollAnime();
 });
